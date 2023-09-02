e6399adda3769bf603d740a97735db742eb3032d
Luna <git@l4.pm>
2023-09-02 18:09:34 -0300

    add codes

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,82 @@
 const std = @import("std");
 
+const c = @cImport({
+    @cInclude("gd.h");
+});
+
+const log = std.log.scoped(.montagd);
+pub extern "c" fn fdopen(fd: c_int) ?*c.FILE;
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var image = c.gdImageCreateTrueColor(1024, 512);
+    defer _ = c.gdImageDestroy(image);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    c.gdImageFill(image, 0, 0, c.gdImageColorAllocateAlpha(image, 0, 0, 0, 127));
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var args_it = std.process.args();
+    _ = args_it.skip(); // skip arg0
+    var a: c_int = 0;
+    var target_file: ?[*:0]const u8 = null;
 
-    try bw.flush(); // don't forget to flush!
-}
+    while (args_it.next()) |arg| {
+        log.debug("arg: {s} ({d} out of {d})", .{
+            arg,
+            args_it.inner.index,
+            args_it.inner.count,
+        });
+        if (std.mem.startsWith(u8, arg, "-")) {
+            // this is an arg, ignore it, and ignore the next one too
+            if (!args_it.skip()) return error.ExpectedOptionToIgnore;
+            log.debug("skip!", .{});
+        } else if (args_it.inner.index == args_it.inner.count) {
+            log.debug("target = {s}", .{arg});
+            target_file = arg;
+        } else {
+            log.debug("processing {s}", .{arg});
+            //var file = try std.fs.openFileAbsolute(arg, .{ .mode = .read_only });
+            //defer file.close();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+            //const c_file = fdopen(file.handle);
+
+            const c_file = std.c.fopen(arg, "r");
+
+            if (c_file == null) return error.FailedToOpenFile;
+            defer _ = if (c_file) |f| std.c.fclose(f);
+
+            log.debug("open", .{});
+            var incoming_image = c.gdImageCreateFromWebp(@as(?*c.FILE, @ptrCast(@alignCast(c_file))));
+            defer c.gdImageDestroy(incoming_image);
+
+            log.debug("alpha blend", .{});
+            c.gdImageAlphaBlending(incoming_image, c.GD_TRUE);
+
+            log.debug("copy resample", .{});
+            c.gdImageCopyResampled(
+                image,
+                incoming_image,
+                @mod(a, @as(c_int, 8)) * 128,
+                (a >> 3) * 128,
+                0,
+                0,
+                128,
+                128,
+                c.gdImageSX(incoming_image),
+                c.gdImageSY(incoming_image),
+            );
+
+            a += 1;
+        }
+    }
+
+    c.gdImageSaveAlpha(image, c.GD_TRUE);
+    log.debug("writing final file {?s}", .{target_file});
+
+    const c_file = std.c.fopen(target_file orelse return error.ExpectedLastArg, "w");
+
+    if (c_file == null) return error.FailLastArg;
+    defer _ = if (c_file) |f| std.c.fclose(f);
+
+    log.debug("open", .{});
+
+    c.gdImagePng(image, @as(?*c.FILE, @ptrCast(@alignCast(c_file))));
 }
